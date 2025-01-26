48b6f955b4975d53e659204c991991ff2081f3bf
LauraHerold <lherold@jhu.edu>
2025-01-26 13:18:19 -0500

    Updated scripts

--- scripts/1_create_maps.py
+++ scripts/1_create_maps.py
@@ -2,9 +2,9 @@ import CMBanom
 
 # Parameters
 nside = 128
-N_start = 1 
-N_maps = 1000
+N_start = 40000
+N_maps = 60000
 cl_fn = "../data/real/COM_PowerSpect_CMB-base-plikHM-TTTEEE-lowl-lowE-lensing-minimum-theory_R3.01.txt"
-outdir = "../data/sims/sims_1k/"
+outdir = "/tank/NoBackup/lherold/maps_100k/"
 
 CMBanom.gen_maps_from_cls(cldatfile=cl_fn, outdir=outdir, Nside=nside, N_start=N_start, N_maps=N_maps, lmax=200, regen=True, returnoutf=False)
--- scripts/2_compute_corr_cls.sh
+++ scripts/2_compute_corr_cls.sh
@@ -1,28 +1,28 @@
 #!/bin/bash                                                                                                                                                                     
 NMIN=0
-NMAX=1000
-OUTDIR="/tank/NoBackup/lherold/sims"
+NMAX=10000
+OUTDIR="/tank/NoBackup/lherold"
 # Full sky
 MASKFILE="None"
 # Common mask
 ##MASKFILE="../data/masks/common-Mask-Int_cutoff0.9_Nside128.fits"
 ##MASKLABEL="commask"
 # Std. mask
-##MASKFILE="../data/masks/stdv_mask_1percent.fits_v2.fits"
+##MASKFILE="../data/masks/stdv_mask_1percent.fits_v3.fits"
 ##MASKLABEL="stdmask"
 
 if [[ "$MASKFILE" == "None" ]]; then
     for N in `seq $NMIN $NMAX`; do
-        MAPFILE="${OUTDIR}/map__${N}.fits"
-        CORFILE="${OUTDIR}/corr_fullsky__${N}.txt"
-        CLFILE="${OUTDIR}/cl_fullsky__${N}.txt"
+        MAPFILE="${OUTDIR}/maps_100k/map__${N}.fits"
+        CORFILE="${OUTDIR}/corrs_fullsky_100k/corr_fullsky__${N}.txt"
+        CLFILE="${OUTDIR}/cls_fullsky_100k/cl_fullsky__${N}.txt"
         spice -mapfile $MAPFILE -corfile $CORFILE -clfile $CLFILE
     done
 else
     for N in `seq $NMIN $NMAX`; do
-        MAPFILE="${OUTDIR}/map__${N}.fits"
-        CORFILE="${OUTDIR}/corr_${MASKLABEL}__${N}.txt"
-        CLFILE="${OUTDIR}/cl_${MASKLABEL}__${N}.txt"
+        MAPFILE="${OUTDIR}/maps_100k/map__${N}.fits"
+        CORFILE="${OUTDIR}/corrs_${MASKLABEL}_100k/corr_${MASKLABEL}__${N}.txt"
+        CLFILE="${OUTDIR}/cls_${MASKLABEL}_100k/cl_${MASKLABEL}__${N}.txt"
         spice -mapfile $MAPFILE -maskfile $MASKFILE -corfile $CORFILE -clfile $CLFILE
     done
 fi
