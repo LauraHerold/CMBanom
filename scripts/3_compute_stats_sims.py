a8ea33ebf45b641067821569c19baada982e49f1
LauraHerold <lherold@jhu.edu>
2025-03-13 09:21:02 -0400

    Updated scripts

--- scripts/3_compute_stats_sims.py
+++ scripts/3_compute_stats_sims.py
@@ -16,29 +16,28 @@ stats_dir = "../data/stats/"
 names_mask = ["fullsky", "stdmask", "commask"]
 mask_files = ["stdv_mask_1percent_v4.fits", "common-Mask-Int_cutoff0.9_Nside128.fits"]
 
+# Modes
+compute_Smu       = False
+compute_R         = False
+compute_sigma16   = False
+compute_SQO       = False
+compute_envelopes = True
+
 ## Cl's and corr's function
-compute_envelopes = False
+percentiles = True
 
 ## Low correlation, Smu
-compute_Smu = True
 summation = True
 mu = 0.5
 
 # Parity asymmetry, R
-compute_R = False
 lmax_R = 60
 
 # Hemispherical asymmetry, sigma16
-compute_sigma16 = False
 ecliptic_coords = True
 if compute_sigma16:
     mask_dir_south_ecl = "mask_south_ecl_Nside16.fits"
     mask_files = ["stdv_mask_1percent_cutoff0.9_Nside16.fits", "common-Mask-Int_cutoff0.9_Nside16.fits"]
-
-
-# Quadrupole-octopole alignment, SQO
-compute_SQO = False
-
 
 if compute_Smu:
     print("Computing Smu:")
@@ -129,18 +128,32 @@ if compute_envelopes:
         print(name_mask, "...")
         
         # Load corrs
-        theta, cos_theta, corrs = CMBanom.load_corrs(corrs_dir, name_mask, Nsims)
+        theta, cos_theta, corrs = CMBanom.load_corrs(corrs_dir+f"corrs_{name_mask}_100k/", name_mask, Nsims)
 
         # Save corr envelope
         mean_corr = np.mean(corrs, axis=0)
-        std_corr = np.std(corrs, axis=0)
-        np.savetxt(stats_dir+"corr_mean_std_"+name_mask+".npy", np.array([mean_corr, std_corr]))
+        if percentiles:
+            perc_68_lower = np.percentile(corrs, (100-68.27)/2, axis=0)
+            perc_68_upper = np.percentile(corrs, 68.27+(100-68.27)/2, axis=0)
+            perc_95_lower = np.percentile(corrs, (100-95.45)/2, axis=0)
+            perc_95_upper = np.percentile(corrs, 95.45+(100-95.45)/2, axis=0)
+            np.savetxt(stats_dir+"corr_mean_perc_"+name_mask+".npy", np.array([mean_corr, perc_68_lower, perc_68_upper, perc_95_lower, perc_95_upper]))
+        else:
+            std_corr = np.std(corrs, axis=0)
+            np.savetxt(stats_dir+"corr_mean_std_"+name_mask+".npy", np.array([mean_corr, std_corr]))            
 
         # Load Cls and correct for window function
         cl_wf_factor = CMBanom.get_cl_wf_factor(Nside_in)
-        cls = CMBanom.load_cls(cls_dir, name_mask, Nsims, cl_wf_factor)
+        cls = CMBanom.load_cls(cls_dir+f"cls_{name_mask}_100k/", name_mask, Nsims, cl_wf_factor)
 
         # Save Cl envelope
         mean_cls = np.mean(cls, axis=0)
-        std_cls = np.std(cls, axis=0)
-        np.savetxt(stats_dir+"cls_mean_std_"+name_mask+".npy", np.array([mean_cls, std_cls]))
+        if percentiles:
+            perc_68_lower = np.percentile(cls, (100-68.27)/2, axis=0)
+            perc_68_upper = np.percentile(cls, 68.27+(100-68.27)/2, axis=0)
+            perc_95_lower = np.percentile(cls, (100-95.45)/2, axis=0)
+            perc_95_upper = np.percentile(cls, 95.45+(100-95.45)/2, axis=0)
+            np.savetxt(stats_dir+"cls_mean_perc_"+name_mask+".npy", np.array([mean_cls, perc_68_lower, perc_68_upper, perc_95_lower, perc_95_upper]))
+        else:
+            std_cls = np.std(cls, axis=0)
+            np.savetxt(stats_dir+"cls_mean_std_"+name_mask+".npy", np.array([mean_cls, std_cls]))
